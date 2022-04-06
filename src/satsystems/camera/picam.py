f1c7a4218e0ad2ac20f6c980f2294b5807cc15cc
Sean Tedesco <stedesco@live.com>
2022-04-06 16:53:11 -0400

    camera: support single shot and video
    Signed-off-by: Sean Tedesco <stedesco@live.com>

--- src/satsystems/camera/picam.py
+++ src/satsystems/camera/picam.py
@@ -1,19 +1,30 @@
 from picamera import PiCamera
 from time import sleep
+from datetime import datetime
 
 class PiCam(PiCamera):
     
-    def __init__(self, resolution=(1024, 768)):
+    def __init__(self, resolution=(1024, 768), framerate:int=16):
         super().__init__()
 
         self.resolution = resolution
+        self.framerate = framerate
 
-    def shot(self, filename: str):
-        output = filename + '.jpg'
+    def shot(self, filename:str='image'):
+        time_stamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        output = filename + time_stamp + '.jpg'
+
         self.start_preview()
         sleep(2)
         self.capture(output)
 
-    def video(self):
-        print('PiCam is recording!')
+    def video(self, filename:str='video'):
+        time_stamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+        output = filename + time_stamp + '.h264'
+
+        self.start_preview()
+        self.start_recording(output)
+        sleep(5)
+        self.stop_recording()
+        self.stop_preview()
     
