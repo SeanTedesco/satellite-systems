43339338fa6956cc2df1105e254e2f5d3a001779
SeanTedesco <stedesco@live.com>
2022-04-28 23:23:43 -0400

    gps: add functional argparser

--- src/satsystems/gps/gps.py
+++ src/satsystems/gps/gps.py
@@ -1,3 +1,5 @@
+import argparse
+
 class Gps:
     '''Interface class to control a GPS.'''
 
@@ -5,20 +7,30 @@ class Gps:
         pass 
 
     def get_location(self, **kwargs):
-        '''Send a message.'''
-        raise NotImplementError('Should be implemented by derived class.')
+        '''Get the current location of the satellite.'''
+        raise NotImplementedError('Should be implemented by derived class.')
 
 
 def parse_cmdline():
-    pass
+    parser = argparse.ArgumentParser(description='Control GPS Module.')
+
+    subparser = parser.add_subparsers()
+
+    location_parser = subparser.add_parser('location', help='Get location data.')
+    #location_parser.add_argument('-d', '--data', type=str, help='The string of data to be transmitted.')
+    location_parser.set_defaults(function=do_location)
+
+    return parser.parse_args()
+
+def do_location(gps, options):
+    gps.get_location()
 
 def main():
     from .grove import Grove
 
-    #options = parse_cmdline()
-    gps = Grove(uid='radio_uid', port='/dev/ttyUSB2', baudrate=115200)
-    gps.get_location()
-    #options.functions(options, gps)
+    options = parse_cmdline()
+    gps = Grove(uid='gps_uid', port='/dev/ttyUSB2', baudrate=115200)
+    options.function(gps, options)
 
 if __name__ == '__main__':
     main()
