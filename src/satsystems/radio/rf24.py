daa139f401c2714625fc64f5a46f2f3f2aba0085
SeanTedesco <stedesco@live.com>
2022-05-02 12:25:01 -0400

    rf24: support beacon and monitor commands

--- src/satsystems/radio/rf24.py
+++ src/satsystems/radio/rf24.py
@@ -3,7 +3,7 @@ from ..logger.logger import SatelliteLogger
 import time
 
 class RF24(Radio):
-    
+
     def __init__(self, port, baud=115200, start_marker='<', end_marker='>'):
         super().__init__(port, baud, start_marker, end_marker)
 
@@ -11,7 +11,7 @@ class RF24(Radio):
         self.stop_receive = 'STOP'                  # message to stop receiving messages
 
     def transmit(self, data:str):
-        '''Send a message.
+        '''Send a single message.
 
         Params:
             data: the message to be transmitted to the other radio.
@@ -35,34 +35,31 @@ class RF24(Radio):
         try:
             self._send_to_arduino(formatted_data)
         except Exception as e:
+            self.radio_logger.warning(f'failed to transmit: {formatted_data}')
             raise e
 
         return data_string_len
 
-    def receive(self, output_file:str='output.log'):
-        '''Receive a message.
+    def receive(self, timeout:int=60):
+        '''Attempt to receive a single message from the arduino.
 
         Params:
-            output_file: the name of the file to save the received data.
+            - Timeout (optional): duration in which to receive a message.
         Return:
-            the number of characters received and saved.
+            - if received, the stripped string sent from the arduino over serial.
+            - if not received, 'xxx'
         '''
+        start_time = time.time()
+        received = 'xxx'
+        while received == 'xxx':
+            received = self._receive_from_arduino().strip()
+            if time.time() > start_time + timeout:
+                self.radio_logger.warning('no message received')
+                break
 
-        received = ''
-        received_count = 0
+        return received
 
-        if not output_file.strip():
-            raise ValueError('No file specified for output.')
-
-        while received != self.stop_receive:
-            received = self._receive_single_message()
-            if not (received == "xxx"):
-                received_count += len(received)
-                self.radio_logger.info(received)
-
-        return received_count
-
-    def command(self, command_code):
+    def command(self, command_code:str):
         '''Send a command/request to the other radio, await for a response.'''
 
         # verify request code
@@ -75,41 +72,42 @@ class RF24(Radio):
         # inform user of success or faiilure  
 
         self.transmit(str(command_code))
-        got_back = self._receive_single_message()
-        self.radio_logger.info(got_back)
+        self.radio_logger.info(f'transmitted command: {command_code}')
+        got_back = self.receive()
+        if got_back == 'xxx':
+            self.radio_logger.info(f'failed to receive acknowledgement')
 
     def stream(self, filename:str):
         '''Stream data in a file.'''
 
         print('rf24 streaming...')
 
-    def beacon(self):
+    def beacon(self, status:str='healthy', pulse_count:int=10):
         '''Transmit a beacon message.'''
 
-        print('rf24 beaconing...')
+        for i in range(pulse_count):
+            self.transmit('VA3TFO')
+            time.sleep(1)
+            self.transmit(status)
+            time.sleep(1)
 
-    def monitor(self):
+    def monitor(self, filename:str):
         '''Constantly listen for a signal.'''
 
-    def _receive_single_message(self, timeout:int=60):
-        '''Attempt to receive a message from the arduino.
-
-        Params:
-            - Timeout (optional): duration in which to receive a message.
-        Raises:
-            - TimeoutError: fails if no message is received from the radio.
-        Return:
-            - the stripped string sent from the arduino over serial.
-        '''
-        start_time = time.time()
         received = 'xxx'
-        while received == 'xxx':
-            received = self._receive_from_arduino().strip()
-            if time.time() > start_time + timeout:
-                self.radio_logger.warning('no message received')
-                raise TimeoutError(f'did not receive message within {timeout}s!')
+        received_count = 0
 
-        return received
+        if not filename.strip():
+            raise FileNotFoundError('No file specified for output monitoring.')
+
+        while received != self.stop_receive:
+            received = self.receive()
+            if not (received == 'xxx'):
+                received_count += len(received)
+                with open(file=filename, mode='a') as f:
+                    f.write(received)
+
+        return received_count
 
     def _format_tx_data(self, data:str):
         '''Formart the data to what the arduino expects for transmissions.
