bbc0a8a447a192b0cee22f0dda11caf1801eac5a
Sean Tedesco <stedesco@live.com>
2022-09-09 09:18:12 -0400

    deployer: ant dbd: use  LEDs as deployment indicators

--- src/satsystems/deployer/ANT_DBD.py
+++ src/satsystems/deployer/ANT_DBD.py
@@ -1,6 +1,7 @@
 from .deployer import Deployer, Deployable
 from typing import List
-
+import time
+from gpiozero import LED
 
 class ANT_DBD(Deployer):
     
@@ -8,17 +9,28 @@ class ANT_DBD(Deployer):
         self.deployer_name = deployer_name
         super().__init__(self.deployer_name)
 
+        self.led_green = LED(22)
+        self.led_yellow = LED(23)
+        self.led_red = LED(24)
+
     def arm_deployment(self):
         '''Prepare the system for deployment.'''
 
         self.logger.critical('arming deployment!')
+        self.led_red.on()
+        time.sleep(1)
     
     def fire_deployment(self, deployables:List[Deployable]):
         '''Deploy the provided system.'''
 
         self.logger.critical('firing deployment!')
+        self.led_red.off()
         for deployable in deployables:
             self.logger.debug(f'deploying: {deployable}')
+            self.led_yellow.on()
+            time.sleep(1)
+            self.led_yellow.off()
+            time.sleep(1)
 
     def detect_deployment(self, deployables:List[Deployable]):
         '''Detect whether the provided systems truly deployed.'''
