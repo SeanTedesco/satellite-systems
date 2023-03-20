148198e63013b4af122711b167f1d5dfd260c018
Sean Tedesco <stedesco@live.com>
2023-03-20 10:01:23 -0400

    src: camera: all: support conversion to and from images and base64 files

--- src/satsystems/camera/camera.py
+++ src/satsystems/camera/camera.py
@@ -13,22 +13,33 @@ def parse_cmdline():
     video_parser.add_argument('-f', '--filename', type=str, help='Name of output file.')
     video_parser.set_defaults(function=do_video)
 
+    converter_parser = subparser.add_parser('convert', help='Convert to and from Images and Base64 strings.')
+    converter_parser.add_argument('-f', '--filename', type=str, help='Path to the image file.')
+    converter_parser.add_argument('-b', '--base64', action='store_true', help='Change to base64 to iamge conversion.')
+    converter_parser.set_defaults(function=do_conversion)
+
     return parser.parse_args()
 
-def do_shot(camera, **kwargs):
-    print(f'do shot with: {kwargs}')
+def do_shot(camera, options):
     camera.shot()
 
-def do_video(camera, **kwargs):
-    print(f'do video with: {kwargs}')
+def do_video(camera, options):
     camera.video()
+
+def do_conversion(camera, options):
+    file_path = options.filename
+    use_base64 = options.base64
+    if use_base64:
+        camera.base64_to_image(file_path)
+    else:
+        camera.image_to_base64(file_path)
 
 def main():
     from .picam import PiCam
 
-    options = parse_cmdline()
+    option = parse_cmdline()
     cam = PiCam()
-    options.function(cam, option=options)
+    option.function(cam, options=option)
 
 if __name__ == '__main__':
     main()
--- src/satsystems/camera/picam.py
+++ src/satsystems/camera/picam.py
@@ -1,10 +1,14 @@
-import sys
+import sys, os, io
 from time import sleep
 from datetime import datetime
 from picamera import PiCamera
+import base64
+from pathlib import Path
+from PIL import Image
+
 
 class PiCam(PiCamera):
-    
+
     def __init__(self, resolution=(1024, 768), framerate:int=16):
         super().__init__()
 
@@ -28,3 +32,19 @@ class PiCam(PiCamera):
         sleep(5)
         self.stop_recording()
         self.stop_preview()
+
+    def image_to_base64(self, input_file:str):
+        output_file = Path(input_file).stem + ".txt"
+        output_path = "./data/" + output_file
+        with open(input_file, "rb") as img_file:
+            b64_string = base64.b64encode(img_file.read())
+        with open(output_path, "wb") as txt_file:
+            txt_file.write(b64_string)
+
+    def base64_to_image(self, input_file:str):
+        output_file = Path(input_file).stem + ".png"
+        output_path = "./data/" + output_file
+        with open(input_file, "r") as text_file:
+                b64_string = base64.b64decode(text_file.read())
+                img = Image.open(io.BytesIO(b64_string))
+                img.save(output_path)
