0cee2c166d222aeafebc4051410fa0516e4db77b
Sean Tedesco <stedesco@live.com>
2023-04-03 07:40:16 -0400

    testing: change to simple i2c read and write

--- testing.py
+++ testing.py
@@ -1,9 +1,17 @@
 import os
+from smbus2 import SMBus
+import platform
+import time
 
 def main():
-    import platform
     my_os = platform.system()
     print("OS in my system : ",my_os)
+ 
+    with SMBus(1) as bus:
+        b = bus.read_i2c_block_data(0x08, 0x00, 4)
+        print(b)
+        time.sleep(2)
+        bus.write_i2c_block_data(0x08, 0x00, [0x05, 0x06, 0x07, 0x08])
 
 if __name__ == '__main__':
     main()
